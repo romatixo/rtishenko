24001e4114856386fc407a7a173412e416b46e7a
romatixo <romatixo1@yandex.ru>
2017-04-13 17:09:49 +0300

    new

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,20 @@
 package ru.job4j;
 
+
+/**
+*Class Calculate решение задачи части 001 урок1.
+*@author rtishenko
+*@since 13.04.2017
+*/
 public class Calculate {
+
+
+/**
+*Основной метод мейn для вывода текста на консоль
+*@param args 
+*/
    public static void main (String[] args) {
-     System.out.println("Hello World!!!");
+    	 System.out.println("Hello World!!!");
    }
+
 }
