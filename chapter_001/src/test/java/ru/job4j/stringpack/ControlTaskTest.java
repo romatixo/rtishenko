108eb89b63f6503e6f26b53fef8c4901e03c0c17
romatixo <romatixo1@yandex.ru>
2017-04-25 22:10:03 +0300

    error

---
+++ chapter_001/src/test/java/ru/job4j/stringpack/ControlTaskTest.java
@@ -0,0 +1,24 @@
+﻿package ru.job4j.stringpack;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+* ControlTaskTest  класс теста слияния двух отсортированных массивов в один отсортированный.
+* @author romatihsenko
+* @since 25.04.2017
+* @version 1
+*/
+public class ControlTaskTest {
+	/**
+	* Тест слияния двух отсортированных массивов в один отсортированный.
+	*/
+    @Test
+    public void when13And24Then1234() {
+       ControlTask control = new ControlTask();
+       int[] result = control.conc(new int[] {1, 3, 5}, new int[] {2, 4, 5, 7});
+       int[] expected = new int[] {1, 2, 3, 4, 5, 5, 7};
+       assertThat(result, is(expected));
+    }
+}
