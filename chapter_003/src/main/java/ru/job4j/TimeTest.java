ecc87c3b2708a18f26643119f41ac98fe5493081
romatixo <romatixo1@yandex.ru>
2017-05-26 01:08:03 +0300

    Collection

--- chapter_003/src/main/java/ru/job4j/TimeTest.java
+++ chapter_003/src/main/java/ru/job4j/TimeTest.java
@@ -1,42 +1,64 @@
 package ru.job4j;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.LinkedList;
-import java.util.TreeSet;
-
-
+import java.util.*;
+/**
+ * TimeTest класс проверки времени.
+ * @author romatihsenko
+ * @since 21.05.2017
+ * @version 1
+ */
 public class TimeTest {
-    private String line = "QWDIQWNHD";
+    /**
+     * Строка.
+     */
+    private String line = "";
 
+    /**
+     * @param collection - коллекция.
+     * @param line - строка
+     * @param amount - колво
+     * @return  время
+     */
     public long add(Collection<String> collection, String line, int amount) {
         long time = System.currentTimeMillis();
+
         for (int i = 0; i < amount; i++) {
+            line += i;
             collection.add(line);
         }
         return System.currentTimeMillis() - time;
     }
-
+    /**
+     * @param collection - коллекция.
+     * @param amount - колво
+     * @return  время
+     */
     public long delete(Collection<String> collection, int amount) {
         long time = System.currentTimeMillis();
+        Iterator<String> iterator = collection.iterator();
         for (int i = 0; i < amount; i++) {
-            collection.remove(this.line);
+            if (iterator.hasNext()) {
+                 iterator.next();
+                iterator.remove();
+            }
         }
-
         return System.currentTimeMillis() - time;
     }
 
+    /**
+     * Метод запуска.
+     */
     public  void go() {
         Collection<String> linkedlist = new LinkedList<String>();
         Collection<String> arraylist = new ArrayList<String>();
         Collection<String> treeset = new TreeSet<String>();
-        long linkedlisttime = this.add(linkedlist, this.line, 500000);
-        long arraylisttime = this.add(arraylist, this.line, 500000);
-        long treesettime = this.add(treeset, this.line, 500000);
+        long linkedlisttime = this.add(linkedlist, this.line, 5000);
+        long arraylisttime = this.add(arraylist, this.line, 5000);
+        long treesettime = this.add(treeset, this.line, 5000);
         System.out.println(String.format("Time to add LinkedList : %s\r\nTime to add ArrayList : %s\r\nTime to add TreeSet : %s\r\n",linkedlisttime, arraylisttime, treesettime ));
-        linkedlisttime = this.delete(linkedlist,  500000);
-        arraylisttime = this.delete(arraylist,500000);
-        treesettime = this.delete(treeset, 500000);
+        linkedlisttime = this.delete(linkedlist,  500);
+        arraylisttime = this.delete(arraylist,500);
+        treesettime = this.delete(treeset, 500);
         System.out.println(String.format("Time to delete LinkedList : %s\r\nTime to delete ArrayList : %s\r\nTime to delete TreeSet : %s\r\n",linkedlisttime, arraylisttime, treesettime ));
 
     }
