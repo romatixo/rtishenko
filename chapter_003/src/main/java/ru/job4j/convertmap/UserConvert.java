971ac4177fe277617db4da770aa6fcfa27cbc2a7
romatixo <romatixo1@yandex.ru>
2017-05-26 17:18:06 +0300

    convertMap

---
+++ chapter_003/src/main/java/ru/job4j/convertmap/UserConvert.java
@@ -0,0 +1,52 @@
+package ru.job4j.convertmap;
+
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+
+public class UserConvert {
+
+    class User {
+        private int id;
+        private String name;
+        private String city;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+    }
+
+    /**
+     * @param list - входной лист.
+     * @return полученная map
+     */
+    public HashMap<Integer, User> process(List<User> list) {
+        HashMap<Integer, User> map = new HashMap<>();
+        Iterator<User> iterator = list.iterator();
+        for (User user : list) {
+            map.put(user.getId(), user);
+        }
+        return map;
+    }
+}
