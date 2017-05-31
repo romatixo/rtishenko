d6b5d3fca6f465640c59699159e8f364795953e9
romatixo <romatixo1@yandex.ru>
2017-05-31 15:12:03 +0300

    Reload

--- chapter_003/src/main/java/ru/job4j/convertmap/SortUser.java
+++ chapter_003/src/main/java/ru/job4j/convertmap/SortUser.java
@@ -7,68 +7,62 @@ package ru.job4j.convertmap;
  * @version 1
  */
 import java.util.*;
+class User implements Comparable<User> {
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
 
+    private Integer age;
+    private String name;
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return age == user.age;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = age != null ? age.hashCode() : 0;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public int compareTo(User o) {
+        return this.age.compareTo(o.age);
+    }
+}
 public class SortUser {
 
-    class User implements Comparable<User> {
-        @Override
-        public String toString() {
-            return "User{" +
-                    "age=" + age +
-                    ", name='" + name + '\'' +
-                    '}';
-        }
 
-        private Integer age;
-        private String name;
-
-        public User(int age, String name) {
-            this.age = age;
-            this.name = name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-
-            User user = (User) o;
-
-            return age == user.age;
-        }
-
-        @Override
-        public int hashCode() {
-            int result = age != null ? age.hashCode() : 0;
-            result = 31 * result + (name != null ? name.hashCode() : 0);
-            return result;
-        }
-
-        @Override
-        public int compareTo(User o) {
-            return this.age.compareTo(o.age);
-        }
-    }
     public List<User> sortHash (List<User> list) {
-        list.sort(new Comparator<User>() {
-            @Override
-            public int compare(User o1, User o2) {
-                return new Integer(o1.hashCode()).compareTo(new Integer(o2.hashCode()));
-            }
-        });
+        list.sort((o1,o2)->Integer.compare(o1.hashCode(),o2.hashCode()));
         return list;
     }
     public List<User> sortLength (List<User> list) {
-        list.sort(new Comparator<User>() {
-            @Override
-            public int compare(User o1, User o2) {
-                return o1.name.length() < o2.name.length() ? o1.name.length() :  o2.name.length();
-            }
-        });
+        list.sort((o1, o2) -> Integer.compare(o1.getName().length(), o2.getName().length()));
         return list;
     }
 
